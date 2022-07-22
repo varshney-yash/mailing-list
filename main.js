c7bef068c0cbff7c11ea1d9088d6396aa67f7127
Yash Varshney <107148830+varshney-yash@users.noreply.github.com>
2022-07-22 22:11:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
-const { faker } = require('@faker-js/faker');
+const express = require('express');
 const mysql=require('mysql');
+const app = express();
+const bodyParser = require("body-parser");
 
-const connection = mysql.createConnection({
+
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+var connection = mysql.createConnection({
 	host:'localhost',
 	user:'root',
 	database:'join_me'
 });
 
-// console.log(faker.internet.email());
 
-// console.log(faker.date.past().toString());
+// test
+// app.listen(3000, function(){
+// 	console.log('Server running on 3000');
+// });
 
-// MySQL Node Package
+// route
+app.get("/", function(req,results){ // function is the callback
+	var q="SELECT COUNT(*) AS count FROM users";
+	connection.query(q,function(error,res){
+		if (error) throw error;
+		var count = res[0].count;
+		// console.log(results);
+		// res.send("You're one of " + count + " unique users registered in my database");
+		results.render("home",{data: count});
+	});
+});
 
-function generateAddress(){
-  console.log(faker.address.streetAddress());
-  console.log(faker.address.city());
-  console.log(faker.address.state());
-}
+app.post("/register", function(req,ressults){
+	req.body.email
+});
 
-generateAddress();
+app.listen(port, function(){
+	console.log('App is listening on port 3000');
+});
+
